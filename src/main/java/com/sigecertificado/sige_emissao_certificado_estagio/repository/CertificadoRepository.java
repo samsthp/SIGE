ea3f8c77c9aee9df4bd43e360af5253a2cda39c4
WesleydeOliveiraSousa <wesley.osousa7@gmail.com>
2025-11-07 10:31:55 -0300

    CSU014 – Emissão de Certificado de Estágio - continuação da implementação da arquitetura de camadas e conexão com o banco de dados

---
+++ src/main/java/com/sigecertificado/sige_emissao_certificado_estagio/repository/CertificadoRepository.java
@@ -0,0 +1,6 @@
+package com.sigecertificado.sige_emissao_certificado_estagio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CertificadoRepository extends JpaRepository {
+}
