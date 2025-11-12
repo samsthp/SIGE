48385d2b27cd03593fa63b99c13c4b0f0035539b
cmaffort <maffort@gmail.com>
2025-11-12 08:31:36 -0300

    Revert "Merge branch 'clone_main' into WesleyDeOliveiraSousa"
    This reverts commit 6a741b0c59859c4962058c2e0fe2159e56f49a66, reversing
    changes made to 3de87c30ecf7575e34804580a33b4c88e389a349.

--- src/main/java/com/sige/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sige.repository;
-
-import com.sige.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
