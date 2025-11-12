3de87c30ecf7575e34804580a33b4c88e389a349
WesleydeOliveiraSousa <wesley.osousa7@gmail.com>
2025-11-12 00:10:49 -0300

    CSU014 – Emissão de Certificado de Estágio - alteração no pom.xml e na aplicação

--- src/main/java/com/sigecertificado/sige_emissao_certificado_estagio/application/CertificadoApplication.java
+++ src/main/java/com/sigecertificado/sige_emissao_certificado_estagio/application/CertificadoApplication.java
@@ -1,4 +1,4 @@
-package com.sigecertificado.sige_emissao_certificado_estagio.applicaation;
+package com.sigecertificado.sige_emissao_certificado_estagio.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
